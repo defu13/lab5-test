31b4e511dbac6e4e39df1056f47c1e8fdf963fc5
Yubal <ydefuente@gmail.com>
2024-04-21 01:26:56 +0200

    añadidas operaciones

--- test_operations.py
+++ test_operations.py
@@ -1,12 +1,27 @@
 import unittest
 
-from operations import sumar
+from operations import sumar, restar, multiplicar, dividir
 
-class TestSumar(unittest.TestCase):
+class TestOperations(unittest.TestCase):
     def test_sumar(self):
         self.assertEqual(sumar(3, 2), 5)
         self.assertEqual(sumar(-1, 1), 0)
         self.assertEqual(sumar(-1, -1), -2)
+
+    def test_restar(self):
+        self.assertEqual(restar(5, 2), 3)
+        self.assertEqual(restar(1, 1), 0)
+        self.assertEqual(restar(10, -5), 15)
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(3, 2), 6)
+        self.assertEqual(multiplicar(-1, 1), -1)
+        self.assertEqual(multiplicar(-1, -1), 1)
+
+    def test_dividir(self):
+        self.assertEqual(dividir(10, 2), 5)
+        self.assertEqual(dividir(5, 0), float('inf'))  # división entre cero
+        self.assertEqual(dividir(0, 5), 0)
 
 if __name__ == '__main__':
     unittest.main()
